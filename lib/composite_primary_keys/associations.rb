815a4fddc9645f854ad1e0ef9f9a821040a4a614
David Palm <dpalm@elctech.com>
2008-10-05 23:38:52 +0200

    CHANGED: replaced String.to_a with [*ary] so Ruby 1.9 works.

--- lib/composite_primary_keys/associations.rb
+++ lib/composite_primary_keys/associations.rb
@@ -81,7 +81,7 @@ module ActiveRecord::Associations::ClassMethods
           collection.target.push(association) unless collection.target.include?(association)
         when :has_one, :belongs_to
           return if record.id.to_s != join.parent.record_id(row).to_s or
-                    join.aliased_primary_key.to_a.any? { |key| row[key].nil? }
+                    [*join.aliased_primary_key].any? { |key| row[key].nil? }
           association = join.instantiate(row)
           record.send("set_#{join.reflection.name}_target", association)
         else
