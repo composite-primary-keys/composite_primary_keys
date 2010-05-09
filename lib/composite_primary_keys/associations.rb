c9ca1aafcd5124b0bdf68b73925b91d48c9adc1a
Charlie Savage <cfis@zerista.com>
2010-05-09 02:36:00 -0600

    Fix up column_names_with_alias - had it in the wrong module.

--- lib/composite_primary_keys/associations.rb
+++ lib/composite_primary_keys/associations.rb
@@ -141,17 +141,19 @@ module ActiveRecord
       end
     end
 
-    class JoinDependency
-      class JoinBase
-        def column_names_with_alias
-          unless defined?(@column_names_with_alias)
-            @column_names_with_alias = []
-            keys = active_record.composite? ? primary_key.map(&:to_s) : [primary_key]
-            (keys + (column_names - keys)).each_with_index do |column_name, i|
-              @column_names_with_alias << [column_name, "#{ aliased_prefix }_r#{ i }"]
+    module ClassMethods
+      class JoinDependency
+        class JoinBase
+          def column_names_with_alias
+            unless defined?(@column_names_with_alias)
+              @column_names_with_alias = []
+              keys = active_record.composite? ? primary_key.map(&:to_s) : [primary_key]
+              (keys + (column_names - keys)).each_with_index do |column_name, i|
+                @column_names_with_alias << [column_name, "#{ aliased_prefix }_r#{ i }"]
+              end
             end
+            @column_names_with_alias
           end
-          @column_names_with_alias
         end
       end
     end
