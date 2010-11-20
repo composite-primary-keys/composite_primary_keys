69d43f909ced34083f466b9c55ccf55debda1e99
Charlie Savage <cfis@zerista.com>
2010-11-20 00:05:45 -0700

    Update history.txt and version again (and learn how to use git better :)

--- lib/composite_primary_keys/version.rb
+++ lib/composite_primary_keys/version.rb
@@ -2,8 +2,8 @@ module CompositePrimaryKeys
   module VERSION #:nodoc:
     MAJOR = 3
     MINOR = 0
-    TINY  = 0
-    PATCH = 'b3'
+    TINY  = 1
+    PATCH = 0
     STRING = [MAJOR, MINOR, TINY, PATCH].join('.')
   end
 end
