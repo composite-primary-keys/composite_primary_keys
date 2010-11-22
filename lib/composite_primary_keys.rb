f79f2779955a4239156ab540ba8ced1c06997dc7
Marian Rudzynski <mr@impaled.org>
2010-11-22 16:14:08 +0100

    made new active_record/attribute_methods/primary_key compatible with composite keys

--- lib/composite_primary_keys.rb
+++ lib/composite_primary_keys.rb
@@ -45,6 +45,7 @@ require 'active_record/associations/has_one_association'
 require 'active_record/associations/has_one_through_association'
 require 'active_record/associations/through_association_scope'
 require 'active_record/relation/query_methods'
+require 'active_record/attribute_methods/primary_key'
 
 require 'composite_primary_keys/fixtures'
 require 'composite_primary_keys/composite_arrays'
@@ -63,6 +64,7 @@ require 'composite_primary_keys/base'
 require 'composite_primary_keys/calculations'
 require 'composite_primary_keys/validations/uniqueness'
 require 'composite_primary_keys/query_methods'
+require 'composite_primary_keys/primary_key'
 
 Dir[File.dirname(__FILE__) + '/composite_primary_keys/connection_adapters/*.rb'].each do |adapter|
   begin
