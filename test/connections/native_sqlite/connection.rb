be16a849827c49939c47bcdd235d7b14cdb55374
toby cabot <toby@caboteria.org>
2010-11-26 16:20:14 -0500

    fixes to help make the "sqlite3:*" rake tasks work
    It looks as if this code had rusted so I tried to bring it up to
    speed, using the mysql and pgsql adapters as examples.  The big
    difference is that for sqlite you need to specify a file location
    instead of user/password.

--- test/connections/native_sqlite/connection.rb
+++ test/connections/native_sqlite/connection.rb
@@ -1,12 +1,12 @@
 print "Using native Sqlite3\n"
 require 'logger'
-require 'adapter_helper/sqlite3'
 require 'active_record'
+require File.join(PROJECT_ROOT, 'test', 'connections', 'connection_spec')
 
 ActiveRecord::Base.logger = Logger.new("debug.log")
 
 def connection_string
-  connection_SPEC['dbfile']
+  SPEC['dbfile']
 end
 
 SPEC = CompositePrimaryKeys::ConnectionSpec[:sqlite3]
