71a046752376f487485118d768a21a5790936c43
Charlie Savage <cfis@zerista.com>
2010-11-28 17:27:24 -0700

    Some updates to patch from FX.

--- lib/composite_primary_keys/primary_key.rb
+++ lib/composite_primary_keys/primary_key.rb
@@ -2,11 +2,16 @@ module ActiveRecord
   module AttributeMethods #:nodoc:
     module PrimaryKey
       def to_key
+        # CPK
+        #key = send(self.class.primary_key)
+        #[key] if key
+
         primary_key = self.class.primary_key
         if primary_key.is_a?(Array)
           primary_key.collect{|k| send(k)}
         else
-          super
+          key = send(primary_key)
+          [key] if key
         end
       end
     end
