f836fa44223679d44154a1a0625080f2ecd6e194
Charlie Savage <cfis@zerista.com>
2010-12-17 22:05:17 -0700

    Update version, update history.

--- lib/composite_primary_keys/version.rb
+++ lib/composite_primary_keys/version.rb
@@ -1,8 +1,8 @@
 module CompositePrimaryKeys
   module VERSION #:nodoc:
     MAJOR = 3
-    MINOR = 0
-    TINY  = 9
+    MINOR = 1
+    TINY  = 0
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
 end
