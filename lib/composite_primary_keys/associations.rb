12782ab102103461280208f11bc85b8e7b423755
Charlie Savage <cfis@zerista.com>
2011-03-23 01:50:09 -0600

    Fix instatiation of composite key records via associations (copy back in 2.x code).

--- lib/composite_primary_keys/associations.rb
+++ lib/composite_primary_keys/associations.rb
@@ -3,6 +3,23 @@ module ActiveRecord
     module ClassMethods
       class JoinDependency
         class JoinBase
+          def aliased_primary_key
+            # CPK
+            # "#{aliased_prefix}_r0"
+
+            active_record.composite? ?
+              primary_key.inject([]) {|aliased_keys, key| aliased_keys << "#{ aliased_prefix }_r#{aliased_keys.length}"} :
+              "#{ aliased_prefix }_r0"
+          end
+
+          def record_id(row)
+            # CPK
+            # row[aliased_primary_key]
+            active_record.composite? ?
+              aliased_primary_key.map {|key| row[key]}.to_composite_keys :
+              row[aliased_primary_key]
+          end
+
           def column_names_with_alias
             unless defined?(@column_names_with_alias)
               @column_names_with_alias = []
