0246e50d98a1b6f20dd28d2b87a24fcf0c7be816
David Rueck <drueck@gmail.com>
2011-05-31 16:56:33 -0700

    Removing models and fixtures no longer needed

--- test/fixtures/product_component.rb
+++
@@ -1,6 +0,0 @@
-class ProductComponent < ActiveRecord::Base
-
-  has_and_belongs_to_many :product_component_roles, :join_table => 'product_roles_components',
-    :foreign_key => :product_component_id, :association_foreign_key => [:product_id, :role_num]
-
-end
