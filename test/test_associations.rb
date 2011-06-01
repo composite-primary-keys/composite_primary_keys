0def798d401f4aaae08ba8301c8f9f85044a6d88
David Rueck <drueck@gmail.com>
2011-05-31 17:00:45 -0700

    Moved tests for HABTM association to separate file and changed to use existing models

--- test/test_associations.rb
+++ test/test_associations.rb
@@ -1,9 +1,9 @@
 require 'abstract_unit'
 
 class TestAssociations < ActiveSupport::TestCase
-  fixtures :articles, :products, :tariffs, :product_tariffs, :suburbs, :streets, :restaurants, :restaurants_suburbs,
+  fixtures :articles, :products, :tariffs, :product_tariffs, :suburbs, :streets, :restaurants,
            :dorms, :rooms, :room_attributes, :room_attribute_assignments, :students, :room_assignments, :users, :readings,
-           :memberships, :product_component_roles, :product_components
+           :memberships
   
   def test_has_many_through_with_conditions_when_through_association_is_not_composite
     user = User.find(:first)
@@ -158,42 +158,6 @@ class TestAssociations < ActiveSupport::TestCase
 
     @suburb = Suburb.find([2, 1], :include => :first_streets)
     assert_equal 1, @suburb.first_streets.size
-  end
-
-  def test_has_and_belongs_to_many
-    @restaurant = Restaurant.find([1,1])
-    assert_equal 2, @restaurant.suburbs.size
-
-    @restaurant = Restaurant.find([1,1], :include => :suburbs)
-    assert_equal 2, @restaurant.suburbs.size
-  end
-
-  def test_hbtm_clear_cpk_both_sides
-    @restaurant = Restaurant.find([1,1])
-    assert_equal 2, @restaurant.suburbs.size
-    @restaurant.suburbs.clear
-    @restaurant = Restaurant.find([1,1])
-    assert_equal 0, @restaurant.suburbs.size
-  end
-
-  def test_hbtm_clear_cpk_owner_side_only
-    role = ProductComponentRole.find([1,1])
-    comp1 = ProductComponent.find(1)
-    comp2 = ProductComponent.find(2)
-    role.product_components << comp1 << comp2
-    assert_equal 2, role.product_components.size
-    role.product_components.clear
-    assert_equal 0, role.product_components.size
-  end
-
-  def test_hbtm_clear_cpk_association_side_only
-    comp = ProductComponent.find(1)
-    role1 = ProductComponentRole.find([1,1])
-    role2 = ProductComponentRole.find([2,1])
-    comp.product_component_roles << role1 << role2
-    assert_equal 2, comp.product_component_roles.size
-    comp.product_component_roles.clear
-    assert_equal 0, comp.product_component_roles.size
   end
 
   def test_has_many_with_primary_key
