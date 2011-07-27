ca22937b5c366bebd77166d01c257ab8408f8c40
Charlie Savage <cfis@zerista.com>
2011-07-26 21:49:18 -0600

    Update to beta6.

--- lib/composite_primary_keys/version.rb
+++ lib/composite_primary_keys/version.rb
@@ -3,6 +3,6 @@ module CompositePrimaryKeys
     MAJOR = 4
     MINOR = 0
     TINY  = 0
-    STRING = [MAJOR, MINOR, TINY, 'beta4'].join('.')
+    STRING = [MAJOR, MINOR, TINY, 'beta6'].join('.')
   end
 end
