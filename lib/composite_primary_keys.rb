27861ad424199c80dd3f8477708ef13c84c00218
Charlie Savage <cfis@zerista.com>
2012-02-12 11:22:41 -0700

    Change copyright to add Charlie Savage.

--- lib/composite_primary_keys.rb
+++ lib/composite_primary_keys.rb
@@ -1,5 +1,5 @@
 #--
-# Copyright (c) 2006 Nic Williams
+# Copyright (c) 2006-2012 Nic Williams and Charlie Savage
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
