cb618ff15868de6a2515cd30875b0910a833c643
Charlie Savage <cfis@zerista.com>
2012-06-03 00:15:57 -0600

    Git update version to 5.0.7

--- test/test_validations.rb
+++ test/test_validations.rb
@@ -1,13 +1,13 @@
 require File.expand_path('../abstract_unit', __FILE__)
 
 class TestValidations < ActiveSupport::TestCase
-  fixtures :seats
+  fixtures :students, :dorms, :rooms, :room_assignments
 
   def test_uniqueness_validation_persisted
-    seat = Seat.find([1,1])
-    assert(seat.valid?)
+    room_assignment = RoomAssignment.find([1, 1, 1])
+    assert(room_assignment.valid?)
 
-    seat.customer = 2
-    assert(!seat.valid?)
+    room_assignment = RoomAssignment.new(:student_id => 1, :dorm_id => 1, :room_id => 2)
+    assert(!room_assignment.valid?)
   end
 end
