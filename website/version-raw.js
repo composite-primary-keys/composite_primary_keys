dd474b45b669e687f1946fba1ba8718ee9d9d05a
Charlie Savage <cfis@zerista.com>
2012-11-21 23:11:34 -0700

    Version 5.0.10

--- website/version-raw.js
+++ website/version-raw.js
@@ -1,3 +1,3 @@
 // Announcement JS file
-var version = 5.0.8;
+var version = 5.0.10;
 MagicAnnouncement.show('compositekeys', version);
--- website/version.js
+++ website/version.js
@@ -1,4 +1,4 @@
 // Version JS file
-var version = 5.0.8;
+var version = 5.0.10;
 
 document.write(" - " + version);
