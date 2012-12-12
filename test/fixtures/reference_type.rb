d121d50f8bba44d7e3fcf86deea304a81e95934e
Dinshaw Gobhai <gobhai@gmail.com>
2012-12-12 12:29:17 -0500

    add test and fix for nested_attributes update

--- test/fixtures/reference_type.rb
+++ test/fixtures/reference_type.rb
@@ -1,7 +1,8 @@
 class ReferenceType < ActiveRecord::Base
   self.primary_key = :reference_type_id
   has_many :reference_codes, :foreign_key => "reference_type_id", :dependent => :destroy
-  
+  accepts_nested_attributes_for :reference_codes
+
   validates_presence_of :type_label, :abbreviation
   validates_uniqueness_of :type_label
 
