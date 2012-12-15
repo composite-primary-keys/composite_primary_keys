ffcd7960aad03620f2d65a30ee3f2d883d220d09
tbeauvais <tbeauvais@constantcontact.com>
2012-12-14 19:21:37 -0500

    override reverse_sql_order to break apart CPKs

--- lib/composite_primary_keys/relation/query_methods.rb
+++ lib/composite_primary_keys/relation/query_methods.rb
@@ -1,17 +1,27 @@
-module ActiveRecord::QueryMethods
-  alias :original_reverse_sql_order :reverse_sql_order
-end
-
 module CompositePrimaryKeys::ActiveRecord::QueryMethods
 
   def reverse_sql_order(order_query)
-    
+    # CPK
+    # order_query = ["#{quoted_table_name}.#{quoted_primary_key} ASC"] if order_query.empty?
+
     # break apart CPKs 
     order_query = primary_key.map do |key|
       "#{quoted_table_name}.#{connection.quote_column_name(key)} ASC"
     end if order_query.empty?
 
-    original_reverse_sql_order(order_query)
+    order_query.map do |o|
+      case o
+      when Arel::Nodes::Ordering
+        o.reverse
+      when String, Symbol
+        o.to_s.split(',').collect do |s|
+          s.strip!
+          s.gsub!(/\sasc\Z/i, ' DESC') || s.gsub!(/\sdesc\Z/i, ' ASC') || s.concat(' DESC')
+        end
+      else
+        o
+      end
+    end.flatten
   end
 
 end
