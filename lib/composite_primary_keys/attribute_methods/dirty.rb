430d6a2418328b74bd4c295472eb0c1dd2c65231
Sammy Larbi <sam@codeodor.com>
2013-03-20 17:51:06 -0500

    use Rails' version unless it applies to CPK

--- lib/composite_primary_keys/attribute_methods/dirty.rb
+++ lib/composite_primary_keys/attribute_methods/dirty.rb
@@ -1,27 +1,12 @@
 module ActiveRecord
   module AttributeMethods
     module Dirty
+      alias_method :rails_dirty_write_attribute, :write_attribute
       def write_attribute(attr, value)
-        # CPK
-        if attr.kind_of?(Array)
-          # A *composite* attribute can't be marked as changed! So do nothing now.
-          # We will come back in here with an *individual* attribute when Write#write_attribute looks through the individual attributes comprising this composite key:
-          # [attr_name, value].transpose.map {|name,val| write_attribute(name, val)}
-        else
-          attr = attr.to_s
-
-          # The attribute already has an unsaved change.
-          if attribute_changed?(attr)
-            old = @changed_attributes[attr]
-            @changed_attributes.delete(attr) unless _field_changed?(attr, old, value)
-          else
-            old = clone_attribute_value(:read_attribute, attr)
-            @changed_attributes[attr] = old if _field_changed?(attr, old, value)
-          end
-        end
-
-        # Carry on.
-        super(attr, value)
+        # CPK: A *composite* attribute can't be marked as changed! So do nothing now.
+        # We will come back in here with an *individual* attribute when Write#write_attribute looks through the individual attributes comprising this composite key:
+        # [attr_name, value].transpose.map {|name,val| write_attribute(name, val)}
+        rails_dirty_write_attribute(attr, value) unless attr.kind_of?(Array)        
       end
     end
   end
