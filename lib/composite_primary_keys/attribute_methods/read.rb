600c5e6f0d0debb95bfb1306a39e8ae1f1564346
Sammy Larbi <sam@codeodor.com>
2013-03-20 17:21:36 -0500

    use the loaded Rails' implementation of the method unless it applies to CPK

--- lib/composite_primary_keys/attribute_methods/read.rb
+++ lib/composite_primary_keys/attribute_methods/read.rb
@@ -25,12 +25,12 @@ module ActiveRecord
         end
       end
 
-      def read_attribute(attr_name)
-        # CPK
+      rails_read_attribute = instance_method(:read_attribute)
+      define_method(:read_attribute) do |attr_name|
         if attr_name.kind_of?(Array)
           attr_name.map {|name| read_attribute(name)}.to_composite_keys
         else
-          self.class.type_cast_attribute(attr_name, @attributes, @attributes_cache)
+          rails_read_attribute.bind(self).(attr_name)
         end
       end
     end
