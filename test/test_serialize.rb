6bcd24831d884e384ad684bfecc308d4b87f1aef
Charlie Savage <cfis@zerista.com>
2013-11-02 22:54:25 -0600

    Add test for #166.

--- test/test_serialize.rb
+++ test/test_serialize.rb
@@ -7,4 +7,9 @@ class TestEqual < ActiveSupport::TestCase
     department = Department.first
     assert_equal('{"department_id":1,"location_id":1}', department.to_json)
   end
+
+  def test_serializable_hash
+    department = Department.first
+    assert_equal({"department_id" => 1,"location_id" => 1}, department.serializable_hash)
+  end
 end
