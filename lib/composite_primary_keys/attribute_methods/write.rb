c0413d9e52ee2ce38247d3da8a8de7ba41a4beb1
Piotr Gębala <piotrek.gebala@gmail.com>
2014-04-11 19:02:37 +0200

    Fix write_attribute method

--- lib/composite_primary_keys/attribute_methods/write.rb
+++ lib/composite_primary_keys/attribute_methods/write.rb
@@ -12,19 +12,21 @@ module ActiveRecord
           value
         else
           attr_name = attr_name.to_s
-          # CPK
-          # attr_name = self.class.primary_key if attr_name == 'id' && self.class.primary_key
           attr_name = self.class.primary_key if attr_name == 'id' && self.class.primary_key && !self.composite?
           @attributes_cache.delete(attr_name)
           column = column_for_attribute(attr_name)
 
-          unless column || @attributes.has_key?(attr_name)
-            ActiveSupport::Deprecation.warn(
-                "You're trying to create an attribute `#{attr_name}'. Writing arbitrary " \
-                "attributes on a model is deprecated. Please just use `attr_writer` etc."
-            )
+          # If we're dealing with a binary column, write the data to the cache
+          # so we don't attempt to typecast multiple times.
+          if column && column.binary?
+            @attributes_cache[attr_name] = value
           end
-          @attributes[attr_name] = type_cast_attribute_for_write(column, value)
+
+          if column || @attributes.has_key?(attr_name)
+            @attributes[attr_name] = type_cast_attribute_for_write(column, value)
+          else
+            raise ActiveModel::MissingAttributeError, "can't write unknown attribute `#{attr_name}'"
+          end
         end
       end
       alias_method :raw_write_attribute, :write_attribute
