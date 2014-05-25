composite-primary-keys/composite_primary_keys

commit 74daa9a6c04fd5783162bc2d9eb387b255ea87c6

Fix up create, some changes for update.

lib/composite_primary_keys/persistence.rb
@@ -4,22 +4,23 @@ module CompositePrimaryKeys
       def relation_for_destroy
         return super unless composite?
         
-        where_hash = {}
         primary_keys = Array(self.class.primary_key)
 
         if primary_keys.empty?
           raise ActiveRecord::CompositeKeyError, "No primary key(s) defined for #{self.class.name}"
         end
 
-        primary_keys.each do |key|
-          where_hash[key.to_s] = self[key]
+        where_hash = primary_keys.inject(Hash.new) do |hash, key|
+          hash[key.to_s] = self[key]
+          hash
         end
 
         relation = self.class.unscoped.where(where_hash)
       end
-      
 
       def touch(name = nil)
+        raise ActiveRecordError, "cannot touch on a new record object" unless persisted?
+
         attributes = timestamp_attributes_for_update_in_model
         attributes << name if name
 
@@ -34,35 +35,17 @@ module CompositePrimaryKeys
 
           changes[self.class.locking_column] = increment_lock if locking_enabled?
 
-          @changed_attributes.except!(*changes.keys)
+          changed_attributes.except!(*changes.keys)
 
-          relation    = self.class.send(:relation)
-          arel_table  = self.class.arel_table
           primary_key = self.class.primary_key
 
+          # CPK
+          #self.class.unscoped.where(primary_key => self[primary_key]).update_all(changes) == 1
           primary_key_predicate = relation.cpk_id_predicate(arel_table, Array(primary_key), Array(id))
-
           self.class.unscoped.where(primary_key_predicate).update_all(changes) == 1
-        end
-      end
-
-      def update_record(attribute_names = @attributes.keys)
-        return super(attribute_names) unless composite?
-
-        klass = self.class
-
-        attributes_with_values = arel_attributes_with_values_for_update(attribute_names)
-        return 0 if attributes_with_values.empty?
-
-        if !can_change_primary_key? and primary_key_changed?
-          raise ActiveRecord::CompositeKeyError, "Cannot update primary key values without ActiveModel::Dirty"
-        elsif primary_key_changed?
-          stmt = klass.unscoped.where(primary_key_was).arel.compile_update(attributes_with_values)
         else
-          stmt = klass.unscoped.where(ids_hash).arel.compile_update(attributes_with_values)
+          true
         end
-
-        klass.connection.update stmt.to_sql
       end
 
       # This override ensures that pkeys are set on the instance when records are created.
@@ -71,29 +54,29 @@ module CompositePrimaryKeys
       # which removes the pkey attrs and also sets updated/created at timestamps
       # For some reason when we overide here we lose dirty!
       # So, for now, timestamps are recorded explicitly
-      def create_record(attribute_names = nil)
-        record_timestamps!
-        attribute_names ||= keys_for_partial_write
-        attributes_values = arel_attributes_with_values_for_create(attribute_names)
-
-        new_id = self.class.unscoped.insert attributes_values
-        self.id = new_id if self.class.primary_key
-
-        @new_record = false
-        id
-      end
-
-      def record_timestamps!
-        if self.record_timestamps
-          current_time = current_time_from_proper_timezone
-
-          all_timestamp_attributes.each do |column|
-            if respond_to?(column) && respond_to?("#{column}=") && self.send(column).nil?
-              write_attribute(column.to_s, current_time)
-            end
-          end
-        end
-      end
+      # def create_record(attribute_names = nil)
+      #   record_timestamps!
+      #   attribute_names ||= keys_for_partial_write
+      #    attributes_values = arel_attributes_with_values_for_create(attribute_names)
+      #
+      #   new_id = self.class.unscoped.insert attributes_values
+      #   self.id = new_id if self.class.primary_key
+      #
+      #   @new_record = false
+      #   id
+      # end
+      #
+      # def record_timestamps!
+      #   if self.record_timestamps
+      #     current_time = current_time_from_proper_timezone
+      #
+      #     all_timestamp_attributes.each do |column|
+      #       if respond_to?(column) && respond_to?("#{column}=") && self.send(column).nil?
+      #         write_attribute(column.to_s, current_time)
+      #       end
+      #     end
+      #   end
+      # end
     end
   end
 end

lib/composite_primary_keys.rb
@@ -33,6 +33,7 @@ end
 # AR files we override
 require 'active_record/counter_cache'
 require 'active_record/fixtures'
+require 'active_record/model_schema'
 require 'active_record/persistence'
 require 'active_record/relation'
 require 'active_record/sanitization'
@@ -70,6 +71,7 @@ require 'composite_primary_keys/core'
 require 'composite_primary_keys/composite_arrays'
 require 'composite_primary_keys/composite_predicates'
 require 'composite_primary_keys/fixtures'
+require 'composite_primary_keys/model_schema'
 require 'composite_primary_keys/relation'
 require 'composite_primary_keys/sanitization'
 require 'composite_primary_keys/version'

lib/composite_primary_keys/associations/has_many_association.rb
@@ -3,15 +3,15 @@ module ActiveRecord
     class HasManyAssociation
       def delete_records(records, method)
         if method == :destroy
-          records.each { |r| r.destroy }
+          records.each(&:destroy!)
           update_counter(-records.length) unless inverse_updates_counter_cache?
         else
-          if records == :all
+          if records == :all || !reflection.klass.primary_key
             scope = self.scope
           else
             # CPK
-            # keys  = records.map { |r| r[reflection.association_primary_key] }
-            # scope = scope.where(reflection.association_primary_key => keys)
+            # scope = self.scope.where(reflection.klass.primary_key => records)
+            # scope = self.scope.where(reflection.klass.primary_key => records)
             table = Arel::Table.new(reflection.table_name)
             and_conditions = records.map do |record|
               eq_conditions = Array(reflection.association_primary_key).map do |name|
@@ -31,21 +31,20 @@ module ActiveRecord
           if method == :delete_all
             update_counter(-scope.delete_all)
           else
-            # CPK
-            # update_counter(-scope.update_all(reflection.foreign_key => nil))
-            updates = Array(reflection.foreign_key).inject(Hash.new) do |hash, name|
-              hash[name] = nil
-              hash
-            end
-            update_counter(-scope.update_all(updates))
+            update_counter(-scope.update_all(reflection.foreign_key => nil))
           end
         end
       end
 
       def foreign_key_present?
-        # CPK
-        # owner.attribute_present?(reflection.association_primary_key)
-        Array(reflection.association_primary_key).all? {|key| owner.attribute_present?(key)}
+        if reflection.klass.primary_key
+          # CPK
+          #owner.attribute_present?(reflection.association_primary_key)
+          owner.attribute_present?(reflection.association_primary_key)
+          Array(reflection.klass.primary_key).all? {|key| owner.attribute_present?(key)}
+        else
+          false
+        end
       end
     end
   end

lib/composite_primary_keys/core.rb
@@ -1,26 +1,16 @@
 module ActiveRecord
   module Core
-    def init_internals_with_cpk
-      init_internals_without_cpk
-      # Remove cpk array from attributes, fixes to_json
-      @attributes.delete(self.class.primary_key) if self.composite?
-    end
-    alias_method_chain :init_internals, :cpk
-
-    def initialize_dup(other)
+    def initialize_dup(other) # :nodoc:
       cloned_attributes = other.clone_attributes(:read_attribute_before_type_cast)
       self.class.initialize_attributes(cloned_attributes, :serialized => false)
-      # CPK
-      # cloned_attributes.delete(self.class.primary_key)
-      Array(self.class.primary_key).each {|key| cloned_attributes.delete(key.to_s)}
-      @attributes = cloned_attributes
-      
-      run_callbacks(:initialize) unless _initialize_callbacks.empty?
 
-      @changed_attributes = {}
-      self.class.column_defaults.each do |attr, orig_value|
-        @changed_attributes[attr] = orig_value if _field_changed?(attr, orig_value, @attributes[attr])
-      end
+      @attributes = cloned_attributes
+
+      # CPK
+      #@attributes[self.class.primary_key] = nil
+      Array(self.class.primary_key).each {|key| @attributes[key] = nil}
+
+      run_callbacks(:initialize) unless _initialize_callbacks.empty?
 
       @aggregation_cache = {}
       @association_cache = {}
@@ -28,7 +18,6 @@ module ActiveRecord
 
       @new_record  = true
 
-      ensure_proper_type
       super
     end
   end

lib/composite_primary_keys/model_schema.rb
@@ -1,0 +1,15 @@
+module ActiveRecord
+  module ModelSchema
+    module ClassMethods
+      def columns
+        @columns ||= connection.schema_cache.columns(table_name).map do |col|
+          col = col.dup
+          # CPK
+          #col.primary = (col.name == primary_key)
+          col.primary = Array(primary_key).include?(col.name)
+          col
+        end
+      end
+    end
+  end
+end

lib/composite_primary_keys/nested_attributes.rb
@@ -12,10 +12,10 @@ module ActiveRecord
       if attributes_collection.is_a? Hash
         keys = attributes_collection.keys
         attributes_collection = if keys.include?('id') || keys.include?(:id)
-          [attributes_collection]
-        else
-          attributes_collection.values
-        end
+                                  [attributes_collection]
+                                else
+                                  attributes_collection.values
+                                end
       end
 
       association = association(association_name)
@@ -50,18 +50,16 @@ module ActiveRecord
           end
         elsif existing_record = existing_records.detect { |record| record.id.to_s == attributes['id'].to_s }
           unless call_reject_if(association_name, attributes)
-            # Make sure we are operatingon the actual object which is in the assocaition's
+            # Make sure we are operating on the actual object which is in the association's
             # proxy_target array (either by finding it, or adding it if not found)
-            target_record = association.target.detect { |record| record == existing_record }
-
+            # Take into account that the proxy_target may have changed due to callbacks
+            target_record = association.target.detect { |record| record.id.to_s == attributes['id'].to_s }
             if target_record
               existing_record = target_record
             else
-              association.add_to_target(existing_record)
+              association.add_to_target(existing_record, :skip_callbacks)
             end
-          end
 
-          if !call_reject_if(association_name, attributes)
             assign_to_or_mark_for_destruction(existing_record, attributes, options[:allow_destroy])
           end
         else

lib/composite_primary_keys/relation.rb
@@ -50,13 +50,17 @@ module ActiveRecord
       class << self
         def where_values_hash
           # CPK adds this so that it finds the Equality nodes beneath the And node:
-          nodes_from_and = with_default_scope.where_values.grep(Arel::Nodes::And).map {|and_node| and_node.children.grep(Arel::Nodes::Equality) }.flatten
-
-          equalities = (nodes_from_and + with_default_scope.where_values.grep(Arel::Nodes::Equality)).find_all { |node|
+          #equalities = where_values.grep(Arel::Nodes::Equality).find_all { |node|
+          equalities = where_values.grep(Arel::Nodes::And).map {|and_node| and_node.children.grep(Arel::Nodes::Equality) }.flatten.find_all { |node|
             node.left.relation.name == table_name
           }
 
-          Hash[equalities.map { |where| [where.left.name, where.right] }]
+          binds = Hash[bind_values.find_all(&:first).map { |column, v| [column.name, v] }]
+
+          Hash[equalities.map { |where|
+            name = where.left.name
+            [name, binds.fetch(name.to_s) { where.right }]
+          }]
         end
       end
     end
@@ -73,5 +77,42 @@ module ActiveRecord
       initialize_copy_without_cpk(other)
       add_cpk_support if klass.composite?
     end
+
+    def update_record(values, id, id_was) # :nodoc:
+      substitutes, binds = substitute_values values
+
+      # CPK
+      um = if self.composite?
+        relation = @klass.unscoped.where(cpk_id_predicate(@klass.arel_table, @klass.primary_key, id_was || id))
+        relation.arel.compile_update(substitutes, @klass.primary_key)
+      else
+        @klass.unscoped.where(@klass.arel_table[@klass.primary_key].eq(id_was || id)).arel.compile_update(substitutes, @klass.primary_key)
+      end
+
+      @klass.connection.update(
+        um,
+        'SQL',
+        binds)
+    end
+
+    # def update_record(attribute_names = @attributes.keys)
+    #   return super(attribute_names) unless composite?
+    #
+    #   klass = self.class
+    #
+    #   attributes_with_values = arel_attributes_with_values_for_update(attribute_names)
+    #   return 0 if attributes_with_values.empty?
+    #
+    #   if !can_change_primary_key? and primary_key_changed?
+    #     raise ActiveRecord::CompositeKeyError, "Cannot update primary key values without ActiveModel::Dirty"
+    #   elsif primary_key_changed?
+    #     stmt = klass.unscoped.where(primary_key_was).arel.compile_update(attributes_with_values)
+    #   else
+    #     stmt = klass.unscoped.where(ids_hash).arel.compile_update(attributes_with_values)
+    #   end
+    #
+    #   klass.connection.update stmt.to_sql
+    # end
+
   end
 end

commit 8bca54ac7615d2876124be0eeb105fe831ee00c2

Fix more breakage from 74daa9a6
Some local variables were removed which we need.

lib/composite_primary_keys/persistence.rb
@@ -37,6 +37,8 @@ module CompositePrimaryKeys
 
           changed_attributes.except!(*changes.keys)
 
+          relation    = self.class.send(:relation)
+          arel_table  = self.class.arel_table
           primary_key = self.class.primary_key
 
           # CPK