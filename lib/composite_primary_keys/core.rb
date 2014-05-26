379a73f799cb1a2dcbc1fd2b847f395dff39d87b
Charlie Savage <cfis@zerista.com>
2014-05-26 00:57:23 -0600

    Don't set id as attribute.

--- lib/composite_primary_keys/core.rb
+++ lib/composite_primary_keys/core.rb
@@ -1,5 +1,28 @@
 module ActiveRecord
   module Core
+    def init_internals
+      pk = self.class.primary_key
+
+      # CPK
+      #@attributes[pk] = nil unless @attributes.key?(pk)
+      unless self.composite?
+        @attributes[pk] = nil unless @attributes.key?(pk)
+      end
+
+      @aggregation_cache        = {}
+      @association_cache        = {}
+      @attributes_cache         = {}
+      @readonly                 = false
+      @destroyed                = false
+      @marked_for_destruction   = false
+      @destroyed_by_association = nil
+      @new_record               = true
+      @txn                      = nil
+      @_start_transaction_state = {}
+      @transaction_state        = nil
+      @reflects_state           = [false]
+    end
+
     def initialize_dup(other) # :nodoc:
       cloned_attributes = other.clone_attributes(:read_attribute_before_type_cast)
       self.class.initialize_attributes(cloned_attributes, :serialized => false)
