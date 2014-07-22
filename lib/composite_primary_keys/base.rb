6d450a7b797c0a5bdd9ed360327357a13360c98a
Charlie Savage <cfis@zerista.com>
2014-07-22 02:16:38 -0600

    Fix Persistence overrides so all included modules are called correctly - this restores callbacks.

--- lib/composite_primary_keys/base.rb
+++ lib/composite_primary_keys/base.rb
@@ -3,8 +3,6 @@ module ActiveRecord
   end
 
   class Base
-    include CompositePrimaryKeys::ActiveRecord::Persistence
-    
     INVALID_FOR_COMPOSITE_KEYS = 'Not appropriate for composite primary keys'
     NOT_IMPLEMENTED_YET        = 'Not implemented for composite primary keys yet'
 
--- lib/composite_primary_keys/persistence.rb
+++ lib/composite_primary_keys/persistence.rb
@@ -1,20 +1,8 @@
-module CompositePrimaryKeys
-  module ActiveRecord
-    module Persistence
-      def relation_for_destroy
-        # CPK
-        #pk         = self.class.primary_key
-        #column     = self.class.columns_hash[pk]
-        #substitute = self.class.connection.substitute_at(column, 0)
-        #relation = self.class.unscoped.where(
-        #  self.class.arel_table[pk].eq(substitute))
-        #relation.bind_values = [[column, id]]
-
-        # run AR's original relation_for_destroy when primary key is not composite
-        if Array(self.class.primary_key).size <= 1
-          return super
-        end
-
+module ActiveRecord
+  module Persistence
+    def relation_for_destroy
+      # CPK
+      if self.composite?
         relation = self.class.unscoped
 
         Array(self.class.primary_key).each_with_index do |key, index|
@@ -25,38 +13,48 @@ module CompositePrimaryKeys
         end
 
         relation
+      else
+        pk         = self.class.primary_key
+        column     = self.class.columns_hash[pk]
+        substitute = self.class.connection.substitute_at(column, 0)
+
+        relation = self.class.unscoped.where(
+          self.class.arel_table[pk].eq(substitute))
+
+        relation.bind_values = [[column, id]]
+        relation
       end
+    end
 
-      def touch(name = nil)
-        raise ActiveRecordError, "cannot touch on a new record object" unless persisted?
+    def touch(name = nil)
+      raise ActiveRecordError, "cannot touch on a new record object" unless persisted?
 
-        attributes = timestamp_attributes_for_update_in_model
-        attributes << name if name
+      attributes = timestamp_attributes_for_update_in_model
+      attributes << name if name
 
-        unless attributes.empty?
-          current_time = current_time_from_proper_timezone
-          changes = {}
+      unless attributes.empty?
+        current_time = current_time_from_proper_timezone
+        changes = {}
 
-          attributes.each do |column|
-            column = column.to_s
-            changes[column] = write_attribute(column, current_time)
-          end
-
-          changes[self.class.locking_column] = increment_lock if locking_enabled?
-
-          changed_attributes.except!(*changes.keys)
-
-          relation    = self.class.send(:relation)
-          arel_table  = self.class.arel_table
-          primary_key = self.class.primary_key
-
-          # CPK
-          #self.class.unscoped.where(primary_key => self[primary_key]).update_all(changes) == 1
-          primary_key_predicate = relation.cpk_id_predicate(arel_table, Array(primary_key), Array(id))
-          self.class.unscoped.where(primary_key_predicate).update_all(changes) == 1
-        else
-          true
+        attributes.each do |column|
+          column = column.to_s
+          changes[column] = write_attribute(column, current_time)
         end
+
+        changes[self.class.locking_column] = increment_lock if locking_enabled?
+
+        changed_attributes.except!(*changes.keys)
+
+        relation    = self.class.send(:relation)
+        arel_table  = self.class.arel_table
+        primary_key = self.class.primary_key
+
+        # CPK
+        #self.class.unscoped.where(primary_key => self[primary_key]).update_all(changes) == 1
+        primary_key_predicate = relation.cpk_id_predicate(arel_table, Array(primary_key), Array(id))
+        self.class.unscoped.where(primary_key_predicate).update_all(changes) == 1
+      else
+        true
       end
     end
   end
