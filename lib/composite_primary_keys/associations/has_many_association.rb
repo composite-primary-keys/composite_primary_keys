97c97a97783a2d90e440e61ef27daea755feeb7c
Sammy Larbi <sam@codeodor.com>
2014-08-27 19:02:19 -0500

    * move has_many_association override code to match AR 4.2.0.beta1

--- lib/composite_primary_keys/associations/has_many_association.rb
+++ lib/composite_primary_keys/associations/has_many_association.rb
@@ -6,7 +6,24 @@ module ActiveRecord
           records.each(&:destroy!)
           update_counter(-records.length) unless inverse_updates_counter_cache?
         else
-          scope = self.scope.where(reflection.klass.primary_key => records)
+          # CPK
+          # scope = self.scope.where(reflection.klass.primary_key => records)
+          table = Arel::Table.new(reflection.table_name)
+          and_conditions = records.map do |record|
+            eq_conditions = Array(reflection.association_primary_key).map do |name|
+              table[name].eq(record[name])
+            end
+            Arel::Nodes::And.new(eq_conditions)
+          end
+
+          condition = and_conditions.shift
+          and_conditions.each do |and_condition|
+            condition = condition.or(and_condition)
+          end
+
+          scope = self.scope.where(condition)
+          # end CPK
+          
           update_counter(-delete_count(method, scope))
         end
       end
@@ -14,8 +31,7 @@ module ActiveRecord
       def foreign_key_present?
         if reflection.klass.primary_key
           # CPK
-          #owner.attribute_present?(reflection.association_primary_key)
-          owner.attribute_present?(reflection.association_primary_key)
+          # owner.attribute_present?(reflection.association_primary_key)
           Array(reflection.klass.primary_key).all? {|key| owner.attribute_present?(key)}
         else
           false
