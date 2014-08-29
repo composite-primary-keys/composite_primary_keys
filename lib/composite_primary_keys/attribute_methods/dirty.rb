5a4a75ecd09876d5c74111b798e4ee71f4ced9d0
Sammy Larbi <sam@codeodor.com>
2014-08-29 12:27:31 -0500

    fix some more tests by uncommenting the write_attribute for CPK attrs

--- lib/composite_primary_keys/attribute_methods/dirty.rb
+++ lib/composite_primary_keys/attribute_methods/dirty.rb
@@ -6,7 +6,7 @@ module ActiveRecord
         if attr.kind_of?(Array)
           # A *composite* attribute can't be marked as changed! So do nothing now.
           # We will come back in here with an *individual* attribute when Write#write_attribute looks through the individual attributes comprising this composite key:
-          # [attr_name, value].transpose.map {|name,val| write_attribute(name, val)}
+          [attr, value].transpose.map {|name,val| write_attribute(name, val)}
         else
           attr = attr.to_s
 
