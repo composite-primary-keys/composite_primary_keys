f154a2c2c9f7287c70e5cc26efa20912387073f0
Sammy Larbi <sam@codeodor.com>
2014-08-30 15:56:51 -0500

    * do not use the PK for read_attribute when reading "id" for composite keys since this makes it impossible to read the "id" attribute of a CPK = [:id, :another_id]

--- lib/composite_primary_keys/attribute_methods/read.rb
+++ lib/composite_primary_keys/attribute_methods/read.rb
@@ -2,11 +2,16 @@ module ActiveRecord
   module AttributeMethods
     module Read
       def read_attribute(attr_name, &block)
+        # CPK
+        # name = attr_name.to_s
+        # name = self.class.primary_key if name == 'id'
+        # @attributes.fetch_value(name, &block)
+        
         if attr_name.kind_of?(Array)
           attr_name.map {|name| read_attribute(name)}.to_composite_keys
         else
           name = attr_name.to_s
-          name = self.class.primary_key if name == 'id'
+          name = self.class.primary_key if name == 'id' && !composite?
           @attributes.fetch_value(name, &block)
         end
       end
