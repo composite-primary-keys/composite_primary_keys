86044231dd687e58470bd9097ad7961822ea138d
Charlie Savage <cfis@zerista.com>
2014-10-06 10:35:17 -0700

    Update to version 4.0.7

--- lib/composite_primary_keys.rb
+++ lib/composite_primary_keys.rb
@@ -26,7 +26,7 @@ $:.unshift(File.dirname(__FILE__)) unless
 
 unless defined?(ActiveRecord)
   require 'rubygems'
-  gem 'activerecord', '~>4.0.0'
+  gem 'activerecord', '~>4.0.6'
   require 'active_record'
 end
 
