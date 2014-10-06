bcb7b37faebce06544d37bdccf266ea6abf09140
Charlie Savage <cfis@zerista.com>
2014-10-06 02:23:01 -0700

    Fix deleting records, uncomment out old tests that caused this issue to be missed. #229.

--- lib/composite_primary_keys/relation.rb
+++ lib/composite_primary_keys/relation.rb
@@ -21,17 +21,21 @@ module ActiveRecord
         include CompositePrimaryKeys::ActiveRecord::QueryMethods
 
         def delete(id_or_array)
-          # Without CPK:
-          # where(primary_key => id_or_array).delete_all
+          # CPK
+          if self.composite?
+            id_or_array = if id_or_array.is_a?(CompositePrimaryKeys::CompositeKeys)
+                            [id_or_array]
+                          else
+                            Array(id_or_array)
+                          end
 
-          id_or_array = if id_or_array.kind_of?(CompositePrimaryKeys::CompositeKeys)
-            [id_or_array]
+            id_or_array.each do |id|
+              # Is the passed in id actually a record?
+              id = id.kind_of?(ActiveRecord::Base) ? id.id : id
+              where(cpk_id_predicate(table, self.primary_key, id)).delete_all
+            end
           else
-            Array(id_or_array)
-          end
-
-          id_or_array.each do |id|
-            where(cpk_id_predicate(table, self.primary_key, id)).delete_all
+            where(primary_key => id_or_array).delete_all
           end
         end
 
