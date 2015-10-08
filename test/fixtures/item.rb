4dc7c6090d46e53845214546430c78b664e416cf
Stefan Katscher <stefan.katscher@ottogroup.com>
2015-10-08 18:12:46 +0200

    Add test for has_and_belongs_to_many association (still failing)

--- test/fixtures/item.rb
+++ test/fixtures/item.rb
@@ -1,5 +1,5 @@
 class Item < ActiveRecord::Base
-  self.primary_keys = :item_no, :item_size
+  self.primary_keys = [:item_no, :item_size]
   has_many :item_attribs, :primary_key => :id, :foreign_key => :item_id
   has_one :item_info, :primary_key => :id, :foreign_key => :item_id
 
--- test/test_has_and_belongs_to_many_ids.rb
+++ test/test_has_and_belongs_to_many_ids.rb
@@ -3,21 +3,23 @@ require File.expand_path('../abstract_unit', __FILE__)
 class TestHasAndBelongsToManyIds < ActiveSupport::TestCase
   fixtures :items, :item_groups
 
-  # def test_basic_stuff
-  #   ig1 = ItemGroup.find([1, 'one'])
-  #   assert_not_nil(ig1)
-  #   assert_equal('first itemgroup', ig1.group_desc)
-  #
-  #   it = Item.find([11111, 10])
-  #   ig1.items << it
-  #   ig1.reload
-  #
-  #   ig1 = ItemGroup.find([1, 'one'])
-  #   assert_equal(1, ig1.items.count)
-  #   assert_equal(Item.find([11111, 10]), ig1.items[0])
-  # end
+  def test_habtm_items_insert
+    i1 = Item.find([11111, 10])
+    i2 = Item.find([11111, 11])
 
-  def test_habtm_items_groups_insert
+    ig1 = ItemGroup.find([1, 'one'])
+    ig2 = ItemGroup.find([2, 'two'])
+
+    ig1.items << i1
+    ig1.items << i2
+    ig2.items << i2
+
+    ig1 = ItemGroup.find([1, 'one'])
+    assert_equal(1, ig1.items.count)
+    assert_equal(Item.find([11111, 10]), ig1.items[0])
+  end
+
+  def test_habtm_item_groups_insert
     i1 = Item.find([11111, 10])
     i2 = Item.find([11111, 11])
 
