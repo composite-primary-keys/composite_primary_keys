dd3a3e0f144dd2abdbce49a806e8b9d0f5fda51f
Stefan Katscher <stefan.katscher@ottogroup.com>
2015-10-09 14:01:44 +0200

    Add fixes for oracle task #327

--- test/connections/native_oracle/connection.rb
+++ test/connections/native_oracle/connection.rb
@@ -3,7 +3,7 @@ print "Using native Oracle\n"
 require File.join(PROJECT_ROOT, 'lib', 'composite_primary_keys')
 
 def connection_string
-  "#{SPEC['username']}/#{SPEC['password']}@#{SPEC['host']}"
+  "#{SPEC['username']}/#{SPEC['password']}@//#{SPEC['host']}:#{SPEC['port']}/#{SPEC['database']}"
 end
 
 # Adapter config setup in locals/database_connections.rb
