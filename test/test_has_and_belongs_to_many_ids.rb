8415fab06aaea33b6a90df7608ecbfb14f07efd7
Stefan Katscher <stefan.katscher@ottogroup.com>
2015-10-09 09:53:24 +0200

    Refine test to also test inverse association

--- test/test_has_and_belongs_to_many_ids.rb
+++ test/test_has_and_belongs_to_many_ids.rb
@@ -14,14 +14,25 @@ class TestHasAndBelongsToManyIds < ActiveSupport::TestCase
     ig1.items << i2
     ig2.items << i2
 
-    ig1 = ItemGroup.find([1, 'one'])
-    assert_equal(2, ig1.items.count)
-    assert_equal(Item.find([11111, 10]), ig1.items[0])
-    assert_equal(Item.find([11111, 11]), ig1.items[1])
+    i1.reload
+    i2.reload
 
-    ig2 = ItemGroup.find([2, 'two'])
+    ig1.reload
+    ig2.reload
+
+    assert_equal(2, ig1.items.count)
+    assert_equal(i1, ig1.items[0])
+    assert_equal(i2, ig1.items[1])
+
     assert_equal(1, ig2.items.count)
-    assert_equal(Item.find([11111, 11]), ig2.items[0])
+    assert_equal(i2, ig2.items[0])
+
+    assert_equal(1, i1.item_groups.count)
+    assert_equal(ig1, i1.item_groups[0])
+
+    assert_equal(2, i2.item_groups.count)
+    assert_equal(ig1, i2.item_groups[0])
+    assert_equal(ig2, i2.item_groups[1])
   end
 
   def test_habtm_item_groups_insert
@@ -35,14 +46,25 @@ class TestHasAndBelongsToManyIds < ActiveSupport::TestCase
     i1.item_groups << ig2
     i2.item_groups << ig2
 
-    i1 = Item.find([11111, 10])
+    i1.reload
+    i2.reload
+
+    ig1.reload
+    ig2.reload
+
     assert_equal(2, i1.item_groups.count)
     assert_equal(ig1, i1.item_groups[0])
     assert_equal(ig2, i1.item_groups[1])
 
-    i2 = Item.find([11111, 11])
     assert_equal(1, i2.item_groups.count)
     assert_equal(ig2, i2.item_groups[0])
+
+    assert_equal(1, ig1.items.count)
+    assert_equal(i1, ig1.items[0])
+
+    assert_equal(2, ig2.items.count)
+    assert_equal(i1, ig2.items[0])
+    assert_equal(i2, ig2.items[1])
   end
 end
 
