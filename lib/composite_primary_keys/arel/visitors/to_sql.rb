ca7788d95cb301e308056640ce82067eb2b0f8f1
ypxing <ypxing@gmail.com>
2016-03-14 15:54:07 +1100

    Arel key for CPK

--- lib/composite_primary_keys/arel/visitors/to_sql.rb
+++ lib/composite_primary_keys/arel/visitors/to_sql.rb
@@ -8,14 +8,28 @@ module Arel
           # CPK
           # collector = visit o.left, collector
           if o.left.respond_to?(:name) && o.left.name.is_a?(Array)
-            collector << "("
-            collector = visit(o.left, collector)
-            collector << ")"
+=begin
+            new_name = o.left.name.each_with_index.map do |field, idx|
+              table_name = idx == 0 ? "" : "#{@connection.quote_table_name(o.left.relation.name)}."
+              "#{table_name}#{@connection.quote_column_name(field)}"
+            end.join(",")
+
+            o.left.name = Arel::Nodes::SqlLiteral.new("#{new_name}")
+=end
+
+           if @connection.adapter_name == "SQLite"
+            collector << 'EXISTS'
+           else
+              collector << "("
+              collector = visit(o.left, collector)
+              collector << ")"
+            end
           else
             collector = visit o.left, collector
           end
 
-          collector << " IN ("
+          collector << " IN" unless @connection.adapter_name == "SQLite"
+          collector << " ("
           visit(o.right, collector) << ")"
         end
       end
