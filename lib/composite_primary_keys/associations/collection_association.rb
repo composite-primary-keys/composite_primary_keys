ebeab49b386d701a8b2def1d052105e182193d7e
Sammy Larbi <sam@codeodor.com>
2016-03-27 08:01:12 -0500

    Convert to using Module#prepend since #alias_method_chain is deprecated

--- lib/composite_primary_keys/associations/collection_association.rb
+++ lib/composite_primary_keys/associations/collection_association.rb
@@ -1,15 +1,11 @@
 module CompositePrimaryKeys
   module CollectionAssociation
-    extend ActiveSupport::Concern
-    included do
-      def get_records_with_cpk_support
-        cpk_applies = (target && target.respond_to?(:composite?) && target.composite?) || (owner && owner.respond_to?(:composite?) && owner.composite?)
-        return scope.to_a if cpk_applies
-        get_records_without_cpk_support
-      end
-      alias_method_chain :get_records, :cpk_support
+    def get_records
+      cpk_applies = (target && target.respond_to?(:composite?) && target.composite?) || (owner && owner.respond_to?(:composite?) && owner.composite?)
+      return scope.to_a if cpk_applies
+      super
     end
   end
 end
 
-ActiveRecord::Associations::CollectionAssociation.send(:include, CompositePrimaryKeys::CollectionAssociation)
+ActiveRecord::Associations::CollectionAssociation.prepend CompositePrimaryKeys::CollectionAssociation
