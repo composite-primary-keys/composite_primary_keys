458dbf49af9ca07ed01badda25683e6b6dade3bd
Sammy Larbi <sam@codeodor.com>
2016-03-28 08:33:57 -0500

    Fix for creating associations by assigning the IDs

--- lib/composite_primary_keys/associations/collection_association.rb
+++ lib/composite_primary_keys/associations/collection_association.rb
@@ -5,6 +5,19 @@ module CompositePrimaryKeys
       return scope.to_a if cpk_applies
       super
     end
+
+    def ids_writer(ids)
+      pk_type = reflection.primary_key_type
+      ids = Array(ids).reject(&:blank?)
+      ids.map! { |i| pk_type.cast(i) }
+      # CPK
+      # records = klass.where(reflection.association_primary_key => ids).index_by do |r|
+      in_predicates = Class.new.extend(CompositePrimaryKeys::Predicates).cpk_in_predicate(klass.arel_table, reflection.association_primary_key, ids)
+      records = klass.where(in_predicates).index_by do |r|
+        reflection.association_primary_key.map{ |k| r.send(k) }
+      end.values_at(*ids)
+      replace(records)
+    end
   end
 end
 
