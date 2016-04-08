b1eb79924215e71de784616eaa66328c9dcf9328
Sammy Larbi <sam@codeodor.com>
2016-04-08 17:18:00 -0500

    :green_heart::green_heart::green_heart: Fix CPK where values hash All tests for postgresql, sqlite3, and mysql run :green_heart: now.

---
+++ lib/composite_primary_keys/relation/where_clause.rb
@@ -0,0 +1,31 @@
+module ActiveRecord
+  class Relation
+    class WhereClause
+      def to_h(table_name = nil)
+        equalities = predicates.grep(Arel::Nodes::Equality)
+
+        # CPK Adds this line, because ours are coming in with AND->{EQUALITY, EQUALITY}
+        equalities = predicates.grep(Arel::Nodes::And).map(&:children).flatten.grep(Arel::Nodes::Equality) if equalities.empty?
+
+        if table_name
+          equalities = equalities.select do |node|
+            node.left.relation.name == table_name
+          end
+        end
+
+        binds = self.binds.map { |attr| [attr.name, attr.value] }.to_h
+
+        equalities.map { |node|
+          name = node.left.name
+          [name, binds.fetch(name.to_s) {
+            case node.right
+            when Array then node.right.map(&:val)
+            when Arel::Nodes::Casted, Arel::Nodes::Quoted
+              node.right.val
+            end
+          }]
+        }.to_h
+      end
+		end
+	end
+end
