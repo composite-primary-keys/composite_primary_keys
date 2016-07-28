3497be5b168f4e81ac0f66700e654429f4833359
Charlie Savage <cfis@zerista.com>
2016-07-27 23:24:03 -0600

    Manually merge in fix for #346.

---
+++ test/test_find_in_batches.rb
@@ -0,0 +1,30 @@
+require File.expand_path('../abstract_unit', __FILE__)
+
+class TestFindInBatches < ActiveSupport::TestCase
+  fixtures :capitols
+
+  def test_in_batches
+    capitols = []
+    Capitol.find_in_batches do |chunk|
+      capitols += chunk.map(&:country)
+    end
+    assert_equal(capitols, ['Canada', 'France', 'Mexico', 'The Netherlands'])
+  end
+
+  def test_in_small_batches
+    capitols = []
+    Capitol.find_in_batches(batch_size: 2) do |chunk|
+        capitols += chunk.map(&:country)
+      end
+
+    assert_equal(capitols, ['Canada', 'France', 'Mexico', 'The Netherlands'])
+  end
+
+  def test_in_one_unit_batch
+    capitols = []
+    Capitol.find_in_batches(batch_size: 1) do |chunk|
+        capitols += chunk.map(&:country)
+      end
+    assert_equal(capitols, ['Canada', 'France', 'Mexico', 'The Netherlands'])
+  end
+end
