6bcc2fd88c849813ec78a9e2a2e7bb9a6821d94b
Charlie Savage <cfis@zerista.com>
2016-12-31 02:23:59 -0800

    Clean up syntax.

--- test/fixtures/db_definitions/postgresql.sql
+++ test/fixtures/db_definitions/postgresql.sql
@@ -41,9 +41,9 @@ create table tariffs (
     tariff_id  int  not null,
     start_date date not null,
     amount     int  default null,
-    primary key (tariff_id, start_date),
     created_at timestamp without time zone NOT NULL,
-    updated_at timestamp without time zone NOT NULL
+    updated_at timestamp without time zone NOT NULL,
+    primary key (tariff_id, start_date)
 );
 
 create table product_tariffs (
