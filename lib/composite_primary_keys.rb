b67c5d80894e4fe6ce1cf239af786f6bbcf0a8c8
Charlie Savage <cfis@zerista.com>
2016-12-31 02:25:52 -0800

    Remove unused code.

--- lib/composite_primary_keys.rb
+++ lib/composite_primary_keys.rb
@@ -67,8 +67,6 @@ require 'active_record/relation/finder_methods'
 require 'active_record/relation/predicate_builder'
 require 'active_record/relation/query_methods'
 
-require 'active_record/validations/uniqueness' unless ENV["TESTING_CPK"] == "true"
-
 # CPK files
 require 'composite_primary_keys/persistence'
 require 'composite_primary_keys/base'
@@ -109,8 +107,6 @@ require 'composite_primary_keys/relation/calculations'
 require 'composite_primary_keys/relation/finder_methods'
 require 'composite_primary_keys/relation/predicate_builder'
 require 'composite_primary_keys/relation/query_methods'
-
-require 'composite_primary_keys/validations/uniqueness'
 
 require 'composite_primary_keys/composite_relation'
 
