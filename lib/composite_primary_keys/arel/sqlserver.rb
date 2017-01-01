f69724f8d43a99b472fd6200f5b88c9749969b14
Charlie Savage <cfis@zerista.com>
2016-12-31 19:18:38 -0800

    Missed sqlserver overrides.

---
+++ lib/composite_primary_keys/arel/sqlserver.rb
@@ -0,0 +1,36 @@
+module Arel
+  module Visitors
+    class SQLServer
+      def make_Fetch_Possible_And_Deterministic o
+        return if o.limit.nil? && o.offset.nil?
+        t = table_From_Statement o
+        pk = primary_Key_From_Table t
+        return unless pk
+        if o.orders.empty?
+          # Prefer deterministic vs a simple `(SELECT NULL)` expr.
+          # CPK
+          #o.orders = [pk.asc]
+          o.orders = pk.map {|a_pk| a_pk.asc}
+        end
+      end
+
+      def primary_Key_From_Table t
+        return unless t
+        column_name = schema_cache.primary_keys(t.name) || column_cache(t.name).first.second.try(:name)
+
+        # CPK
+        # column_name ? t[column_name] : nil
+        case column_name
+          when Array
+            column_name.map do |name|
+              t[name]
+            end
+          when NilClass
+            [t[column_name]]
+          else
+            nil
+        end
+      end
+    end
+  end
+end
