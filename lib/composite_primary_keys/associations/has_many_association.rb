c1280ea2f5dd4d738580b82272437cc6f3b2289e
Charlie Savage <cfis@zerista.com>
2017-05-21 02:17:05 -0600

    Fix indentation.

--- lib/composite_primary_keys/associations/has_many_association.rb
+++ lib/composite_primary_keys/associations/has_many_association.rb
@@ -18,29 +18,29 @@ module ActiveRecord
         update_counter(-delete_count(method, scope))
       end
 
-        def delete_count(method, scope)
-          if method == :delete_all
-            scope.delete_all
-          else
-            # CPK
-            # scope.update_all(reflection.foreign_key => nil)
-            conds = Array(reflection.foreign_key).inject(Hash.new) do |mem, key|
-              mem[key] = nil
-              mem
-            end
-            scope.update_all(conds)
+      def delete_count(method, scope)
+        if method == :delete_all
+          scope.delete_all
+        else
+          # CPK
+          # scope.update_all(reflection.foreign_key => nil)
+          conds = Array(reflection.foreign_key).inject(Hash.new) do |mem, key|
+            mem[key] = nil
+            mem
           end
+          scope.update_all(conds)
         end
+      end
 
-        def foreign_key_present?
-          if reflection.klass.primary_key
-            # CPK
-            # owner.attribute_present?(reflection.association_primary_key)
-            Array(reflection.klass.primary_key).all? {|key| owner.attribute_present?(key)}
-          else
-            false
-          end
+      def foreign_key_present?
+        if reflection.klass.primary_key
+          # CPK
+          # owner.attribute_present?(reflection.association_primary_key)
+          Array(reflection.klass.primary_key).all? {|key| owner.attribute_present?(key)}
+        else
+          false
         end
       end
     end
+  end
 end
