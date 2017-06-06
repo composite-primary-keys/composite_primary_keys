7f4670b54b3c6e94992161b4efe2c8717d7c0e71
Jordan Owens <jkowens@gmail.com>
2017-06-06 11:06:46 -0400

    Fix updating primary key

--- lib/composite_primary_keys/attribute_methods/primary_key.rb
+++ lib/composite_primary_keys/attribute_methods/primary_key.rb
@@ -16,6 +16,19 @@ module ActiveRecord
           end
 
         end
+
+        def id_in_database
+          sync_with_transaction_state
+          # CPK
+          # attribute_in_database(self.class.primary_key)
+          if self.composite?
+            self.class.primary_keys.map do |key_attr|
+              attribute_changed?(key_attr) ? changed_attributes[key_attr] : self.ids_hash[key_attr]
+            end
+          else
+            attribute_in_database(self.class.primary_key)
+          end
+        end
       end
     end
   end
