951dee1ba28e5ccf2197ef7d5544c190664fcede
Charlie Savage <cfis@zerista.com>
2017-10-08 17:23:36 -0700

    Try to be a bit closer to original active record method.

--- lib/composite_primary_keys/attribute_methods/primary_key.rb
+++ lib/composite_primary_keys/attribute_methods/primary_key.rb
@@ -23,7 +23,7 @@ module ActiveRecord
           # attribute_in_database(self.class.primary_key)
           if self.composite?
             self.class.primary_keys.map do |key_attr|
-              attribute_changed?(key_attr) ? changed_attributes[key_attr] : self.ids_hash[key_attr]
+              attribute_in_database(key_attr)
             end
           else
             attribute_in_database(self.class.primary_key)
