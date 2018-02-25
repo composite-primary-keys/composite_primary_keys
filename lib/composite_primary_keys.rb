7c3b99d3be3bab8d71964e87a3bbc7942dd8858e
Charlie Savage <cfis@zerista.com>
2018-02-25 13:45:20 -0700

    Update gem requirements.

--- lib/composite_primary_keys.rb
+++ lib/composite_primary_keys.rb
@@ -26,7 +26,7 @@ $:.unshift(File.dirname(__FILE__)) unless
 
 unless defined?(ActiveRecord)
   require 'rubygems'
-  gem 'activerecord', ['~>5.1.0', '>= 5.1.4']
+  gem 'activerecord', ['~>5.1.0', '>= 5.1.5']
   require 'active_record'
 end
 
