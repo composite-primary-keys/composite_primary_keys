d989a51def2519f89a0b4483120a340670bb783b
Charlie Savage <cfis@zerista.com>
2018-02-25 18:40:41 -0700

    Fix mysql failure.

--- lib/composite_primary_keys/connection_adapters/abstract_mysql_adapter.rb
+++ lib/composite_primary_keys/connection_adapters/abstract_mysql_adapter.rb
@@ -2,20 +2,27 @@ module ActiveRecord
   module ConnectionAdapters
     class AbstractMysqlAdapter
       def subquery_for(key, select)
-        subsubselect = select.clone
-        subsubselect.projections = [key]
+        subselect = select.clone
+        subselect.projections = [key]
 
         # Materialize subquery by adding distinct
         # to work with MySQL 5.7.6 which sets optimizer_switch='derived_merge=on'
-        subsubselect.distinct unless select.limit || select.offset || select.orders.any?
+        subselect.distinct unless select.limit || select.offset || select.orders.any?
 
-        subselect = Arel::SelectManager.new(select.engine)
+        # CPK
+        if key.is_a?(Array)
+          key_names = key.map(&:name).map {|key_name| Arel.sql(key_name)}
+          Arel::SelectManager.new(subselect.as("__active_record_temp")).project(key_names)
+        else
+          key_name = quote_column_name(key.name)
+          Arel::SelectManager.new(subselect.as("__active_record_temp")).project(Arel.sql(key_name))
+        end
 
         # CPK
         #subselect.project Arel.sql(key.name)
-        subselect.project Arel.sql(Array(key).map(&:name).join(', '))
+#        subselect.project Arel.sql(Array(key).map(&:name).join(', '))
 
-        subselect.from subsubselect.as('__active_record_temp')
+ #       subselect.from subsubselect.as('__active_record_temp')
       end
     end
   end
