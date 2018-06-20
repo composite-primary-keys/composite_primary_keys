c10c488ea95bf265ed4a4a0f6f7a4e8226b9b7da
Jordan Owens <jkowens@gmail.com>
2018-06-20 14:40:50 -0400

    Apply patch to fix AR 5.0.7

--- lib/composite_primary_keys/locking/optimistic.rb
+++ lib/composite_primary_keys/locking/optimistic.rb
@@ -5,7 +5,7 @@ module ActiveRecord
       private
 
       silence_warnings do
-        def _update_record(attribute_names = @attributes.keys) #:nodoc:
+        def _update_record(attribute_names = self.attribute_names) #:nodoc:
           return super unless locking_enabled?
           return 0 if attribute_names.empty?
 
@@ -25,18 +25,18 @@ module ActiveRecord
               ).where(
                   lock_col => previous_lock_value
               ).update_all(
-                  Hash[attributes_for_update(attribute_names).map do |name|
-                         [name, _read_attribute(name)]
-                       end]
+                  attributes_for_update(attribute_names).map do |name|
+                      [name, _read_attribute(name)]
+                  end.to_h
               )
             else
               affected_rows = relation.where(
                   self.class.primary_key => id,
                   lock_col => previous_lock_value,
               ).update_all(
-                  Hash[attributes_for_update(attribute_names).map do |name|
-                       [name, _read_attribute(name)]
-                     end]
+                  attributes_for_update(attribute_names).map do |name|
+                      [name, _read_attribute(name)]
+                  end.to_h
               )
             end
 
