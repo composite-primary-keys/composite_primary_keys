c164d19ba0bb5d8a25d9678af38c4fa50a37d9be
Charlie Savage <cfis@zerista.com>
2018-11-14 02:38:34 -0800

    Cleanup of #433 on master branch.

--- lib/composite_primary_keys/associations/preloader/association.rb
+++ lib/composite_primary_keys/associations/preloader/association.rb
@@ -14,62 +14,6 @@ module ActiveRecord
           end
         end
 
-        def associated_records_by_owner(preloader)
-          owners_map = owners_by_key
-          # CPK
-          # owner_keys = owners_map.keys.compact
-          owner_keys = if reflection.foreign_key.is_a?(Array)
-            owners.map do |owner|
-              Array(owner_key_name).map do |owner_key|
-                owner[owner_key]
-              end
-            end.compact.uniq
-          else
-            owners_map.keys.compact
-          end
-
-          # Each record may have multiple owners, and vice-versa
-          records_by_owner = owners.each_with_object({}) do |owner,h|
-            h[owner] = []
-          end
-
-          if owner_keys.any?
-            # Some databases impose a limit on the number of ids in a list (in Oracle it's 1000)
-            # Make several smaller queries if necessary or make one query if the adapter supports it
-            sliced  = owner_keys.each_slice(klass.connection.in_clause_length || owner_keys.size)
-
-            records = load_slices sliced
-            records.each do |record, owner_key|
-              owners_map[owner_key].each do |owner|
-                records_by_owner[owner] << record
-              end
-            end
-          end
-
-          records_by_owner
-        end
-
-        def load_slices(slices)
-          @preloaded_records = slices.flat_map { |slice|
-            records_for(slice)
-          }
-
-          # CPK
-          # @preloaded_records.map { |record|
-          #   key = record[association_key_name]
-          #   key = key.to_s if key_conversion_required?
-          #
-          #   [record, key]
-          # }
-          @preloaded_records.map { |record|
-            key = Array(association_key_name).map do |key_name|
-              record[key_name]
-            end.join(CompositePrimaryKeys::ID_SEP)
-
-            [record, key]
-          }
-        end
-
         def owners_by_key
           unless defined?(@owners_by_key)
             @owners_by_key = owners.each_with_object({}) do |owner, h|
