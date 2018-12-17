61f300dcd91254c11ba9db62dc51cbe7e4fad977
Brad Slaughter <brad_slaughter@hotmail.com>
2018-12-17 12:23:55 -0800

    Move construct_join_attributes override to ActiveRecord::Associations::ThroughAssociation

--- lib/composite_primary_keys/associations/has_many_through_association.rb
+++ lib/composite_primary_keys/associations/has_many_through_association.rb
@@ -1,26 +1,7 @@
 module ActiveRecord
   module Associations
     class HasManyThroughAssociation
-      def cpk_join_through_predicate(*records)
-        ensure_mutable
 
-        ids = records.map do |record|
-          source_reflection.association_primary_key(reflection.klass).map do |key|
-            record.send(key)
-          end
-        end
-
-        cpk_in_predicate(through_association.scope.klass.arel_table, source_reflection.foreign_key, ids)
-      end
-
-      # See ActiveRecord::Associations::HasManyThroughAssociation#delete_records for usage
-      def construct_join_attributes(*records)
-        if source_reflection.klass.composite?
-          cpk_join_through_predicate(*records)
-        else
-          super
-        end
-      end
 
       def through_records_for(record)
         # CPK
