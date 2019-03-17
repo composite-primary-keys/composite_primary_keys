ff689aa4f3bd403dea6c154dd2cb1be173ec904e
Charlie Savage <cfis@zerista.com>
2019-03-16 19:38:23 -0700

    Fix join associations.

--- lib/composite_primary_keys/reflection.rb
+++ lib/composite_primary_keys/reflection.rb
@@ -1,19 +1,28 @@
 module ActiveRecord
   module Reflection
     class AbstractReflection
-      def build_join_constraint(table, foreign_table)
+      def join_scope(table, foreign_table, foreign_klass)
+        predicate_builder = predicate_builder(table)
+        scope_chain_items = join_scopes(table, predicate_builder)
+        klass_scope       = klass_join_scope(table, predicate_builder)
+
         key         = join_keys.key
         foreign_key = join_keys.foreign_key
 
         # CPK
-        #constraint = table[key].eq(foreign_table[foreign_key])
+        #klass_scope.where!(table[key].eq(foreign_table[foreign_key]))
         constraint = cpk_join_predicate(table, key, foreign_table, foreign_key)
+        klass_scope.where!(constraint)
+
+        if type
+          klass_scope.where!(type => foreign_klass.polymorphic_name)
+        end
 
         if klass.finder_needs_type_condition?
-          table.create_and([constraint, klass.send(:type_condition, table)])
-        else
-          constraint
+          klass_scope.where!(klass.send(:type_condition, table))
         end
+
+        scope_chain_items.inject(klass_scope, &:merge!)
       end
     end
   end
