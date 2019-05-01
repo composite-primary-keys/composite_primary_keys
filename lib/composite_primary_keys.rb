721742c60c166f9091b1086fa5af0f41c2806ee7
Alexandru Anca <aanca@freshbooks.com>
2019-04-30 21:33:19 -0500

    Use latest Rails 6 rc1
    Rails 6.rc1 fixed an issue about preloading associations [here](https://github.com/rails/rails/pull/35496)
    I have ported over the changes to the Association class

--- lib/composite_primary_keys.rb
+++ lib/composite_primary_keys.rb
@@ -26,7 +26,7 @@ $:.unshift(File.dirname(__FILE__)) unless
 
 unless defined?(ActiveRecord)
   require 'rubygems'
-  gem 'activerecord', '6.0.0.beta3'
+  gem 'activerecord', '6.0.0.rc1'
   require 'active_record'
 end
 
