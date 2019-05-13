127728a070e1a27c1c617b492e9f0a6a7c9d3d12
Charlie Savage <cfis@zerista.com>
2019-05-12 22:40:24 -0600

    Version 12.0.0.rc2

--- lib/composite_primary_keys/version.rb
+++ lib/composite_primary_keys/version.rb
@@ -3,6 +3,6 @@ module CompositePrimaryKeys
     MAJOR = 12
     MINOR = 0
     TINY  = 0
-    STRING = [MAJOR, MINOR, TINY, 'rc1'].join('.')
+    STRING = [MAJOR, MINOR, TINY, 'rc2'].join('.')
   end
 end
