14a67f48e9d731d5b4492864e3aeb7666b74484c
Daniel de Haas <daniel.andrew.dehaas@gmail.com>
2019-10-23 12:04:06 -0700

    Update the comment referring to the overridden Rails code

--- lib/composite_primary_keys/attribute_methods.rb
+++ lib/composite_primary_keys/attribute_methods.rb
@@ -2,7 +2,7 @@ module ActiveRecord
   module AttributeMethods
     def has_attribute?(attr_name)
       # CPK
-      # attributes.key?(attr_name)
+      # @attributes.key?(attr_name.to_s)
       Array(attr_name).all? {|single_attr| attributes.key?(single_attr.to_s) }
     end
   end
