2dbd7a9bbebc7f584c6ec618c7666e1a9a2e4231
Charlie Savage <cfis@savagexi.com>
2020-01-01 14:03:17 -0800

    Merge pull request #506 from Slike9/fix-505-pk-with-comma
    Fix issue #505 pk with comma
    # Conflicts:
    #	.travis.yml
    #	composite_primary_keys.gemspec
    #	lib/composite_primary_keys/associations/preloader/association.rb
    #	lib/composite_primary_keys/relation/finder_methods.rb

--- lib/composite_primary_keys/composite_arrays.rb
+++ lib/composite_primary_keys/composite_arrays.rb
@@ -1,6 +1,7 @@
 module CompositePrimaryKeys
   ID_SEP     = ','
   ID_SET_SEP = ';'
+  ESCAPE_CHAR = '^'
 
   module ArrayExtension
     def to_composite_keys
@@ -8,12 +9,27 @@ module CompositePrimaryKeys
     end
   end
 
-  def self.normalize(ids)
+  # Convert mixed representation of CPKs (by strings or arrays) to normalized
+  # representation (just by arrays).
+  #
+  # `ids` is Array that may contain:
+  # 1. A CPK represented by an array or a string.
+  # 2. An array of CPKs represented by arrays or strings.
+  #
+  # There is an issue. Let `ids` contain an array with serveral strings. We can't distinguish case 1
+  # from case 2 there in general. E.g. the item can be an array containing appropriate number of strings,
+  # and each string can contain appropriate number of commas. We consider case 2 to win there.
+  def self.normalize(ids, cpk_size)
     ids.map do |id|
-      if id.is_a?(Array)
-        normalize(id)
-      elsif id.is_a?(String) && id.index(ID_SEP)
-        id.split(ID_SEP)
+      if Utils.cpk_as_array?(id, cpk_size) && id.any? { |item| !Utils.cpk_as_string?(item, cpk_size) }
+        # CPK as an array - case 1
+        id
+      elsif id.is_a?(Array)
+        # An array of CPKs - case 2
+        normalize(id, cpk_size)
+      elsif id.is_a?(String)
+        # CPK as a string - case 1
+        CompositeKeys.parse(id)
       else
         id
       end
@@ -27,7 +43,7 @@ module CompositePrimaryKeys
       when Array
         value.to_composite_keys
       when String
-        self.new(value.split(ID_SEP))
+        value.split(ID_SEP).map { |key| Utils.unescape_string_key(key) }.to_composite_keys
       else
         raise(ArgumentError, "Unsupported type: #{value}")
       end
@@ -43,9 +59,36 @@ module CompositePrimaryKeys
 
     def to_s
       # Doing this makes it easier to parse Base#[](attr_name)
-      join(ID_SEP)
+      map { |key| Utils.escape_string_key(key.to_s) }.join(ID_SEP)
     end
   end
+
+  module Utils
+    class << self
+      def escape_string_key(key)
+        key.gsub(Regexp.union(ESCAPE_CHAR, ID_SEP)) do |unsafe|
+          "#{ESCAPE_CHAR}#{unsafe.ord.to_s(16).upcase}"
+        end
+      end
+
+      def unescape_string_key(key)
+        key.gsub(/#{Regexp.escape(ESCAPE_CHAR)}[0-9a-fA-F]{2}/) do |escaped|
+          char = escaped.slice(1, 2).hex.chr
+          (char == ESCAPE_CHAR || char == ID_SEP) ? char : escaped
+        end
+      end
+
+      def cpk_as_array?(value, pk_size)
+        # We don't permit Array to be an element of CPK.
+        value.is_a?(Array) && value.size == pk_size && value.none? { |item| item.is_a?(Array) }
+      end
+
+      def cpk_as_string?(value, pk_size)
+        value.is_a?(String) && value.count(ID_SEP) == pk_size - 1
+      end
+    end
+  end
+  private_constant :Utils
 end
 
 Array.send(:include, CompositePrimaryKeys::ArrayExtension)
