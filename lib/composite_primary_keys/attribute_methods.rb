62656b2d86df8c53f097431ea8c59534e1fd6ff0
Nicholas Guarino <me@ndguarino.com>
2020-09-14 13:45:10 -0400

    Implement raw access for attributes
       * Prevents infinite loops with gems which modify the 'attributes' method.

--- lib/composite_primary_keys/attribute_methods.rb
+++ lib/composite_primary_keys/attribute_methods.rb
@@ -3,7 +3,7 @@ module ActiveRecord
     def has_attribute?(attr_name)
       # CPK
       # @attributes.key?(attr_name.to_s)
-      Array(attr_name).all? {|single_attr| attributes.key?(single_attr.to_s) }
+      Array(attr_name).all? {|single_attr| @attributes.key?(single_attr.to_s) }
     end
   end
 end
