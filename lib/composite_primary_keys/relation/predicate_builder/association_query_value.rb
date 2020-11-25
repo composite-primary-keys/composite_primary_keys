419472c90821f24d2cf7349b2bf86bc93ea2ef8c
Sammy Larbi <sam@codeodor.com>
2020-11-25 12:38:57 -0600

    Remove places where Rails converts keys to_s Also update some CPK code that called old methods in Rails

--- lib/composite_primary_keys/relation/predicate_builder/association_query_value.rb
+++ lib/composite_primary_keys/relation/predicate_builder/association_query_value.rb
@@ -3,16 +3,16 @@ module ActiveRecord
     class AssociationQueryValue
       def queries
         # CPK
-        if associated_table.association_join_foreign_key.is_a?(Array)
+        if associated_table.join_foreign_key.is_a?(Array)
           if ids.is_a?(ActiveRecord::Relation)
             ids.map do |id|
-              associated_table.association_join_foreign_key.zip(id.id).to_h
+              associated_table.join_foreign_key.zip(id.id).to_h
             end
           else
-            [associated_table.association_join_foreign_key.zip(ids).to_h]
+            [associated_table.join_foreign_key.zip(ids).to_h]
           end
         else
-          [associated_table.association_join_foreign_key.to_s => ids]
+          [associated_table.join_foreign_key => ids]
         end
       end
     end
