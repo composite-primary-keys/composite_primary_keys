1b2672eb6267000333d9e31c99f7fccfcde6d58e
Charlie Savage <cfis@savagexi.com>
2021-02-16 19:51:17 -0800

    Revert "Be a bit smarter about figuring out what auto incremented field was updated with mysql and mariadb."
    This reverts commit ffca1c5aef7f1e8e748f18191a0ca9bf3b942e53.
    
    # Conflicts:
    #	lib/composite_primary_keys/connection_adapters/abstract/database_statements.rb

--- lib/composite_primary_keys/connection_adapters/abstract/database_statements.rb
+++ lib/composite_primary_keys/connection_adapters/abstract/database_statements.rb
@@ -5,31 +5,16 @@ module ActiveRecord
         sql, binds = to_sql_and_binds(arel, binds)
         value = exec_insert(sql, name, binds, pk, sequence_name)
 
-        return id_value if id_value
-
         if pk.is_a?(Array) && !value.empty?
           # This is a CPK model and the query result is not empty. Thus we can figure out the new ids for each
           # auto incremented field
-          pk.map {|key| value.first[key]}
+          id_value || pk.map {|key| value.first[key]}
         elsif pk.is_a?(Array)
-          # This is CPK, but we don't know what autoincremented fields were updated.
-          result = Array.new(pk.size)
-
-          # Is there an autoincrementing field?
-          auto_key = pk.find do |key|
-            attribute = arel.ast.relation[key]
-            column = column_for_attribute(attribute)
-            if column.respond_to?(:auto_increment?)
-              column.auto_increment?
-            end
-          end
-
-          if auto_key
-            result[pk.index(auto_key)] = last_inserted_id(value)
-          end
-          result
+          # This is CPK, but we don't know what autoincremented fields were updated. So return nil, which means
+          # the existing id_value of the model will be used.
+          id_value || Array.new(pk.size)
         else
-          last_inserted_id(value)
+          id_value || last_inserted_id(value)
         end
       end
     end
