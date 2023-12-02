41f81c8044776bc8d9c69b071386050e8dc391c1
T.S <thebigmercury@gmail.com>
2023-12-02 14:22:23 +0900

    Add test for nested_attribute

--- test/test_nested_attributes.rb
+++ test/test_nested_attributes.rb
@@ -64,4 +64,27 @@ class TestNestedAttributes < ActiveSupport::TestCase
     assert_equal(reference_code.code_label, 'XX')
     assert_equal(reference_code.abbreviation, 'Xx')
   end
+
+  def test_nested_atttribute_update_4
+    code_id = 1003
+
+    reference_type = reference_types(:name_prefix)
+    reference_type.update :reference_codes_attributes => [{
+      :reference_code => code_id,
+      :code_label => 'XX',
+      :abbreviation => 'Xx'
+    }]
+    assert_not_nil ReferenceCode.find_by_reference_code(code_id)
+    reference_code = ReferenceCode.find_by_reference_code(code_id)
+    # directly pass :id as a array 
+    reference_type.update :reference_codes_attributes => [{
+      :id => [reference_type.reference_type_id, code_id],
+      :code_label => 'AAA',
+      :abbreviation => 'Aaa'
+    }]
+    
+    reference_code = ReferenceCode.find_by_reference_code(code_id)
+    assert_kind_of(ReferenceCode, reference_code)
+    assert_equal(reference_code.code_label, 'AAA')
+  end
 end
